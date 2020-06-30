1d2e6f1a71c05b6d70bd0fb948e1525c42c26b5f
ColoSRJones <colosrjones@gmail.com>
2020-06-30 00:39:08 -0500

    Updates

---
+++ precache-manifest.f8173ba0f6f1905ed03703c554960e1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd2067822e8172f641fed18bf76a6870",
+    "url": "/Covid_19_Tracker/index.html"
+  },
+  {
+    "revision": "c8cf82fa4d15e6e88ece",
+    "url": "/Covid_19_Tracker/static/css/main.f626f0b4.chunk.css"
+  },
+  {
+    "revision": "4f7a967bd562a95d7ed7",
+    "url": "/Covid_19_Tracker/static/js/2.db6e948b.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid_19_Tracker/static/js/2.db6e948b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8cf82fa4d15e6e88ece",
+    "url": "/Covid_19_Tracker/static/js/main.3ed21ede.chunk.js"
+  },
+  {
+    "revision": "41b12db25041ec08126f",
+    "url": "/Covid_19_Tracker/static/js/runtime-main.4a7ea13d.js"
+  },
+  {
+    "revision": "31ff4aecd8fec0c9591eab7871239634",
+    "url": "/Covid_19_Tracker/static/media/corona-4930541_1920.31ff4aec.jpg"
+  }
+]);
